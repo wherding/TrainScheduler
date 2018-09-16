eca1c56a48315fc8b86abb0c2aaaf359f6916090
wherding <wherding@outlook.com>
2018-09-16 00:02:06 -0700

    populate table with info form db. next need to work with moment to calc time

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 var database = firebase.database();
-console.log(database);
+//console.log(database);
 
 //inside of my on click event add info to the database
 /*$(".submit").on("click", function(){
@@ -14,7 +14,7 @@ $(document).on("click", ".submit", function(){
     let dest = $("#train-destination").val();
     let ftime = $("#train-time").val();
     let freq = $("#train-frequency").val();
-    console.log("this is my train destination: "+ freq);
+   // console.log("this is my train destination: "+ freq);
     database.ref('trains/').push({
         name: name,
         destination: dest,
@@ -22,5 +22,34 @@ $(document).on("click", ".submit", function(){
         frequency:freq
         //end set
     });
+    populateTable();
  //end click funciton   
 });
+
+
+//begin on change event to update table from database. 
+var populateTable = function(){
+    tableRef = database.ref("trains/")
+    tableRef.on('value', function(snapshot){
+ snapshot.forEach(function(child){
+     var key = child.key;
+   //  console.log("this is my key"+key)
+     var childv= child;
+     console.log(childv)
+     var tr = $("<tr>");
+     var nametd =$("<td>");
+     var destinationtd =$("<td>");
+     var timetd =$("<td>");
+     var freqtd =$("<td>");
+     nametd.text(child.val().name);
+     destinationtd.text(child.val().destination)
+     timetd.text(child.val().ftime)
+     freqtd.text(child.val().frequency)
+     tr.append(nametd,destinationtd,freqtd);
+     $(".traintable").append(tr)
+
+
+ })
+    })
+}
+
